dcd7ad06c5f69ded5159a992850e0e1e3293d736
Marvin Danig <marvin@marvindanig.com>
2017-10-30 15:41:56 -0400

    Intermediate checkin #2:

--- static/js/invoker.js
+++ static/js/invoker.js
@@ -70,7 +70,7 @@ const superbook = Flippy.init(node, settings)
 // superbook.flippy('addPage', pageObj, 3)
 
 // THE BOOK
-// console.log(superbook)
+console.log(superbook)
 // }, true)
 
 /** ********** Z' END **************/
